f4328214ff35b59de446ff99b4fc1c3f2c3fd4bb
DaviDio <david.cascella.5@gmail.com>
2024-09-28 18:15:26 +0200

    modified pages that need auth

---
+++ src/main/java/it/cascella/security/config/ProjectSecurityConfiguration.java
@@ -0,0 +1,24 @@
+package it.cascella.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class ProjectSecurityConfiguration {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/account","balance").authenticated()//necessitano del form login NON NECESSITANO DEL /
+                .requestMatchers("/cards","/contact").permitAll()//non necessitano di autenticazione
+        );
+        http.formLogin(withDefaults());
+        //http.formLogin(flc -> flc.disable()); //this will disable the form login
+
+        http.httpBasic(withDefaults());
+        //http.httpBasic(hbc -> hbc.disable()); //this will disable the http basic authentication
+        return http.build();
+    }
+}
