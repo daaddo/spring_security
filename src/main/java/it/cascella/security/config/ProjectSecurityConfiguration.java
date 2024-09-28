7516a33cd81a0a7693638cc8c034ffa5c47399cd
DaviDio <david.cascella.5@gmail.com>
2024-09-28 19:14:10 +0200

    changed password in their crypted ones

--- src/main/java/it/cascella/security/config/ProjectSecurityConfiguration.java
+++ src/main/java/it/cascella/security/config/ProjectSecurityConfiguration.java
@@ -3,6 +3,13 @@ package it.cascella.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -20,5 +27,25 @@ public class ProjectSecurityConfiguration {
         http.httpBasic(withDefaults());
         //http.httpBasic(hbc -> hbc.disable()); //this will disable the http basic authentication
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService getUserDetailsService() {
+        UserDetails user = User
+                .withUsername("user")
+                .password("{bcrypt}$2a$12$7NGgXefpV2x3CGMfJRTZO.CNGMbjNTmsbKFLrr20YewvpTda9XqCK")
+                .authorities("read")
+                .build();
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("password")
+                .authorities("read")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
